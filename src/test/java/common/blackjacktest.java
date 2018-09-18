8b14c14e69e5a86eafd2be948b5621c72c525269
Jacob Dallaire <jacobdallaire@yahoo.ca>
2018-09-18 14:34:02 -0400

    added test plan

---
+++ src/test/java/common/blackjacktest.java
@@ -0,0 +1,152 @@
+package common;
+
+import junit.framework.TestCase;
+
+public class blackjacktest extends TestCase {
+	
+	public void testFileInput() {
+		
+	}
+	
+	public void testConsoleInput() {
+		
+	}
+	
+	public void testInputChoice() {
+		
+	}
+	
+	public void testPlayerVisibleCards() {
+		
+	}
+	
+	public void testDealerVisibleCards() {
+		
+	}
+	
+	public void testPlayerHit() {
+		
+	}
+	
+	public void testPlayerHitRepeated() {
+		
+	}
+	
+	public void testPlayerStand() {
+		
+	}
+	
+	public void testPlayerHandDisplay() {
+		
+	}
+	
+	public void testPlayerBust() {
+		
+	}
+	
+	public void testDealer16less() {
+		
+	}
+	
+	public void testDealerSoft17() {
+		
+	}
+	
+	public void testDealerHitRepeated() {
+		
+	}
+	
+	public void testDealerHandDisplay() {
+		
+	}
+	
+	public void testDealerBust() {
+		
+	}
+	
+	public void testAceLow() {
+		
+	}
+	
+	public void testAceHigh() {
+		
+	}
+	
+	public void testTwoAces() {
+		
+	}
+	
+	public void testAceHighToLow() {
+		
+	}
+	
+	public void testTwoAcesLow() {
+		
+	}
+	
+	public void testRoyals() {
+		
+	}
+	
+	public void testPlayerBlackjack() {
+		
+	}
+	
+	public void testDealerBlackjack() {
+		
+	}
+	
+	public void testPlayerBlackjackWin() {
+		
+	}
+	
+	public void testDealerBlackjackWin() {
+		
+	}
+	
+	public void testPlayerScore() {
+		
+	}
+	
+	public void testDealerScore() {
+		
+	}
+	
+	public void testPlayerWin() {
+		
+	}
+	
+	public void testDealerWin() {
+		
+	}
+	
+	public void testPlayerSplit() {
+		
+	}
+	
+	public void testDealerSplit() {
+		
+	}
+	
+	public void testPlayerSplitHit() {
+		
+	}
+	
+	public void testDealerSplitHit() {
+		
+	}
+	
+	public void testPlayerSplitBestHand() {
+		
+	}
+	
+	public void testDealererSplitBesthand() {
+		
+	}
+	
+	public void testSplitWinner() {
+		
+	}
+	
+
+}
